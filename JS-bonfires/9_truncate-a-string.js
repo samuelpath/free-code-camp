e48b41556a6e694bb0c081331aec043debdcdcaf
Samuel Path <samuel.path@gmail.com>
2017-05-14 14:53:10 +0200

    new ex

---
+++ JS-bonfires/9_truncate-a-string.js
@@ -0,0 +1,17 @@
+/*
+  Truncate a string (first argument) if it is longer than the given maximum string length (second argument).
+  Return the truncated string with a ... ending.
+  Note that inserting the three dots to the end will add to the string length.
+  However, if the given maximum string length num is less than or equal to 3, 
+  then the addition of the three dots does not add to the string length in determining the truncated string.
+*/
+
+function truncateString(str, num) {
+  if (num >= str.length) {
+    return str;
+  }
+
+  return (num <= 3)
+    ? str.slice(0, num) + "..."
+    : str.slice(0, num - 3) + "...";
+}
