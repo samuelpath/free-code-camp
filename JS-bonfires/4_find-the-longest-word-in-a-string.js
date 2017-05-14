2a7e3eab1b9ba50159747936e049aae08f7599aa
Samuel Path <samuel.path@gmail.com>
2017-05-14 14:09:10 +0200

    improved ex4

--- JS-bonfires/4_find-the-longest-word-in-a-string.js
+++ JS-bonfires/4_find-the-longest-word-in-a-string.js
@@ -1,16 +1,16 @@
+/*
+	Return the length of the longest word in the provided sentence.
+	Your response should be a number.
+*/
 
-function findLongestWord(str) {
-  const wordsInString = str.split(' ');
-  
-  const longestWord = wordsInString.reduce((acc, cur) => {
-	if (cur.length > acc.length) {
-		return cur;
-	} else {
-		return acc;
-	}
-  }, '');
-
-  return longestWord;
+function findLongestWord(word1, word2) {
+	return (word1.length > word2.length) ? word1 : word2;
 }
 
-findLongestWord("The quick brown fox jumped over the lazy dog");
+function findLongestWord(sentence) {
+  const wordsInSentence = sentence.split(' ');
+  const longestWord = wordsInSentence.reduce(getTheLongestWord, '');
+  return longestWord.length;
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
