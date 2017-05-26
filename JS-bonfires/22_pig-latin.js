e0f4c79a5aed718d76cd083ca1a7e9ea362d1136
Samuel Path <samuel.path@gmail.com>
2017-05-26 16:40:36 +0200

    2 nex ex

---
+++ JS-bonfires/22_pig-latin.js
@@ -0,0 +1,29 @@
+/*
+  Translate the provided string to pig latin.
+  Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+  If a word begins with a vowel you just add "way" to the end.
+  Input strings are guaranteed to be English words in all lowercase.
+*/
+
+function translatePigLatin(strToTranslate) {
+  let translatedStr = '';
+  const intermediarySuffix = 'w';
+  const finalSuffix = 'ay';
+  
+  if (isVowel(strToTranslate[0])) {
+    translatedStr = strToTranslate + intermediarySuffix;
+  } else {
+    const initialStrChars = strToTranslate.split('');
+    while (!isVowel(initialStrChars[0])) {
+      initialStrChars.push(initialStrChars.shift(''));
+    }
+    translatedStr = initialStrChars.join('');
+  }
+
+  return translatedStr + finalSuffix;
+}
+
+function isVowel(c) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    return vowels.indexOf(c) !== -1;
+}
