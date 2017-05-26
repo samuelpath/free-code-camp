32642ec88e7b0fad0578024c3e8bd5934fcd287f
Samuel Path <samuel.path@gmail.com>
2017-05-26 17:29:21 +0200

    new ex

---
+++ JS-bonfires/30_sum-all-primes.js
@@ -0,0 +1,32 @@
+/*
+  Sum all the prime numbers up to and including the provided number.
+  A prime number is defined as a number greater than one and having only two divisors, one and itself.
+  For example, 2 is a prime number because it's only divisible by one and two.
+  The provided number may not be a prime.
+*/
+
+function sumPrimes(num) {
+  let sum = 0;
+  
+  for (let i = 0; i <= num; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+function isPrime(num) {
+    if (num < 2) {
+      return false;
+    }
+    
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+
+    return true;
+}
