0eed6d9aa46af841475c9dee2bc19de4940569cb
Samuel Path <samuel.path@gmail.com>
2017-05-29 11:25:25 +0200

    quick display fix

--- JS-bonfires/32_finders-keepers.js
+++ JS-bonfires/32_finders-keepers.js
@@ -1,5 +1,6 @@
 /*
-  Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+  Create a function that looks through an array (first argument) 
+  and returns the first element in the array that passes a truth test (second argument).
 */
 
 const findElement = (arr, func) => arr.filter(func)[0];
