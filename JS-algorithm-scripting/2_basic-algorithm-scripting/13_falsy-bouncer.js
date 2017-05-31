5f3fd47729af4955d72658e9198dd54b63984b13
Samuel Path <samuel.path@gmail.com>
2017-05-31 13:07:12 +0200

    put basic challenges in folder

--- JS-algorithm-scripting/2_basic-algorithm-scripting/13_falsy-bouncer.js
+++
@@ -1,8 +0,0 @@
-/*
-  Remove all falsy values from an array.
-  Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
-*/
-
-function removeFalsyValuesFromArray(arr) {
-  return arr.filter(Boolean);
-}
--- JS-algorithm-scripting/3_intermediate-algorithm-scripting/25_boo-who.js
+++
@@ -1,7 +0,0 @@
-/*
-  Check if a value is classified as a boolean primitive.
-  Return true or false.
-  Boolean primitives are true and false.
-*/
-
-const isBooleanPrimitive = (valueToTest) => typeof valueToTest === 'boolean';
