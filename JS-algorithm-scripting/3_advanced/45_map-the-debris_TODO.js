4e5e69aedea948c8a4bce007e4b9d5bcbab7e684
Samuel Path <samuel.path@gmail.com>
2017-05-31 14:11:24 +0200

    imported instructions

---
+++ JS-algorithm-scripting/3_advanced/45_map-the-debris_TODO.js
@@ -0,0 +1,8 @@
+/*
+  Return a new array that transforms the element's average altitude into their orbital periods.
+  The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+  You can read about orbital periods on wikipedia.
+  The values should be rounded to the nearest whole number. The body being orbited is Earth.
+  The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+*/
+
